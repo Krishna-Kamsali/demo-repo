754ba9d8c04e7a06994da599f7e34bfcda4d989e
Krishna Kumar <krishnakamsali2002@gmail.com>
2023-11-06 18:10:22 +0530

    Basic Java Datatypes

---
+++ Test.java
@@ -0,0 +1,31 @@
+class Test
+{
+ public static void main(String[] args)
+ {
+  //byte
+  byte x = 23;
+  System.out.println(x);
+  //short
+  short s = 3801;
+  System.out.println(s);
+  // integer
+  int a =20;
+  System.out.println(a);
+  //long 
+  long l = 979832680927l;
+  System.out.println(l);
+  //float
+  float f = 32.0f;
+  System.out.println(f);
+  //Double
+  double b = 30.0;
+  System.out.println(b);
+  //char
+  char c = 'w';
+  System.out.println(c);
+  //boolean
+  boolean b1 = true;
+  System.out.println(b1);
+  
+ }
+}
